083bd60fc0f3ee0f991cb1f4c8fcab4661ccbecf
Brewerjg <grammerjasonl@gmail.com>
2023-12-15 10:46:58 -0600

    added sections

--- newsApplication/views.py
+++ newsApplication/views.py
@@ -48,5 +48,50 @@ def index(request):
         urlToImage.append(f['urlToImage'])
         url.append(f['url'])
     busNews = zip(title, description, urlToImage, url)
+    
+    r = requests.get('https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=d4c18b0cb8854efbbbdcf18221e930fc')
+    res = r.json()
+    data = res.get("articles")
+    title = []
+    description = []
+    urlToImage = []
+    url = []
+    for i in range(len(data)):
+        f = data[i]
+        title.append(f['title'])
+        description.append(f['description'])
+        urlToImage.append(f['urlToImage'])
+        url.append(f['url'])
+    sciNews = zip(title, description, urlToImage, url)
+    
+    r = requests.get('https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=d4c18b0cb8854efbbbdcf18221e930fc')
+    res = r.json()
+    data = res.get("articles")
+    title = []
+    description = []
+    urlToImage = []
+    url = []
+    for i in range(len(data)):
+        f = data[i]
+        title.append(f['title'])
+        description.append(f['description'])
+        urlToImage.append(f['urlToImage'])
+        url.append(f['url'])
+    entNews = zip(title, description, urlToImage, url)
+    
+    r = requests.get('https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=d4c18b0cb8854efbbbdcf18221e930fc')
+    res = r.json()
+    data = res.get("articles")
+    title = []
+    description = []
+    urlToImage = []
+    url = []
+    for i in range(len(data)):
+        f = data[i]
+        title.append(f['title'])
+        description.append(f['description'])
+        urlToImage.append(f['urlToImage'])
+        url.append(f['url'])
+    sportNews = zip(title, description, urlToImage, url)
 
-    return render(request, 'newsApplication/index.html', context={'news': news, 'techNews': techNews, 'busNews': busNews})
+    return render(request, 'newsApplication/index.html', context={'news': news, 'techNews': techNews, 'busNews': busNews, 'sciNews': sciNews, 'entNews': entNews, 'sportNews': sportNews})
